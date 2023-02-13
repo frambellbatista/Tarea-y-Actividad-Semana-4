a1df5d4c0bd4cc29e0681f7366877faeeede24ee
Frambel Batista <123329476+frambellbatista@users.noreply.github.com>
2023-02-12 22:30:47 -0400

    Add files via upload

---
+++ Assets/Mainmenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Jugar(){
+		SceneManager.LoadScene("Juego");
+	}
+
+		public void Opciones(){
+		SceneManager.LoadScene("Opciones");
+	}
+
+	public void Salir(){
+		Application.Quit();
+	}
+
+}
